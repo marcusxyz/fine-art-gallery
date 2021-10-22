5e38eee4c551b40219e768fb4ebade0d6b7241fd
Marcus Hägerstrand <marcus.hagerstrand@gmail.com>
2021-10-22 11:11:17 +0200

    added a footer

--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,22 @@
 <section>
     <div class="separator footer"></div>
+    <div class="contact-us">
+        <h2>Do you want to collaborate with us?</h2>
+        <button>
+            Submit your work
+        </button>
+    </div>
+    <div class="separator footer"></div>
+    <div class="links-and-copyright">
+        <ul>
+            <li>
+                <a href="#">Instagram</a>
+                <a href="#">Facebook</a>
+                <a href="#">LinkedIn</a>
+            </li>
+        </ul>
+        <p>©2021</p>
+    </div>
 </section>
 </body>
 
--- php/index.php
+++ php/index.php
@@ -4,9 +4,6 @@
     <div class="hero block-1">
         <h1>
             <?= $indexH1; ?>
-            <!-- Berzelii Gallery
-            <br>
-            Dept. -->
         </h1>
     </div>
     <div class="hero block-2">
@@ -28,94 +25,18 @@
 
 <section class="gallery">
     <div class="container">
-        <div class="box box-1">
-            <div class="frame">
-                <img src="../images/the-woman-of-samaria.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1860</p>
-                <p class="title">The Woman of Samaria</p>
-                <p class="artist">Artist: William Dyce</p>
-            </div>
-        </div>
-
-        <div class="box box-2">
-            <div class="frame">
-                <img src="../images/festive-gathering.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1860</p>
-                <p class="title">Festive Gathering</p>
-                <p class="artist">Artist: William Dyce</p>
-            </div>
-        </div>
-
-        <div class="box box-3">
-            <div class="frame">
-                <img src="../images/hope-comforting-love.jpeg" alt="">
-            </div>
-            <div class="">
-                <p class="era">1901</p>
-                <p class="title">Hope Comforting Love</p>
-                <p class="artist">Artist: Unknown</p>
-            </div>
-        </div>
-
-        <div class="box box-4">
-            <div class="frame">
-                <img src="../images/the-grotto-at-posillipo.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1754</p>
-                <p class="title">The Grotto at Posillipo</p>
-                <p class="artist">Artist: Unknown</p>
-            </div>
-        </div>
-
-        <div class="box box-5">
-            <div class="frame">
-                <img src="../images/banquet-still-life.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1664</p>
-                <p class="title">Banquet Still Life</p>
-                <p class="artist">Artist: Unknown</p>
-
-            </div>
-        </div>
-
-        <div class="box box-6">
-            <div class="frame">
-                <img src="../images/Monte-Civetta.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1867</p>
-                <p class="title">Monte Civetta</p>
-                <p class="artist">Artist: Elijah Walton</p>
-            </div>
-        </div>
-
-        <div class="box box-7">
-            <div class="frame">
-                <img src="../images/the-river-servern-at-shrewsbury.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1770</p>
-                <p class="title">The River Severn at Shrewsbury</p>
-                <p class="artist">Artist: Paul Sandby</p>
-            </div>
-        </div>
-
-        <div class="box box-8">
-            <div class="frame">
-                <img src="../images/sea-lions.jpeg" alt="">
-            </div>
-            <div>
-                <p class="era">1788</p>
-                <p class="title">Monte Civetta</p>
-                <p class="artist">Artist: Charles Collins</p>
-            </div>
-        </div>
+        <?php foreach ($artworks as $artwork) : ?>
+            <a href=<?= $artwork['website']; ?> class="box box-1" target="_blank">
+                <div class="frame">
+                    <img <?= $artwork['image']; ?>>
+                </div>
+                <div>
+                    <p class="era"><?= $artwork['era']; ?></p>
+                    <p class="title"><?= $artwork['title']; ?></p>
+                    <p class="artist"><?= $artwork['artist']; ?></p>
+                </div>
+            </a>
+        <?php endforeach; ?>
     </div>
 </section>
 
